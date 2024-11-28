472b216a69347e9e5b84bd8a63b489a8b1f5fc0d
SleepynSleep <wangx@ibp.ac.cn>
2024-11-28 11:26:56 +0800

    add & push by SleepynSleep using Matlab

--- wxyz/wxyz_git_add_push.m
+++ wxyz/wxyz_git_add_push.m
@@ -1,5 +1,13 @@
-function wxyz_git_add_push
+function wxyz_git_add_push(varargin)
+if nargin == 1 && (ischar(varargin{1})||isstring(varargin{1}))
+    commit = varargin{1};
+elseif nargin == 0
+    commit = "add & push by SleepynSleep using Matlab";
+else
+    error('Please check your input! The input should be string');
+end
 !git add .
-!git commit -m "add & push by SleepynSleep using Matlab"
+% !git commit -m "add & push by SleepynSleep using Matlab"
+system(strcat('git commit -m "', commit, '"'))
 !git push -u origin master
 end
